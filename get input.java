9a3bdefbe5c6fbc21d30d6b7c8438f0f485117b7
Thrideep <97669773+ThrideepS@users.noreply.github.com>
2024-07-10 11:52:28 +0530

    Create get input.java

---
+++ get input.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = input.nextInt();
+        System.out.println("You entered " + number);
+        input.close();
+    }
+}
+
+Output :
+
+Enter an integer: 10
+You entered 10
